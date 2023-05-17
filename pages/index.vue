6aba589261cf86961c3fd9039c6a75d23de3f417
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-05-17 16:16:23 +0900

    Feat: 메인 페이지 레이아웃 구성

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="main">
+		<h1>Conner 4 Cuts</h1>
+		<NuxtLink to="/studio">
+			<button class="start-btn btn">Press to Start</button>
+		</NuxtLink>
+	</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+	.main {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		h1 {
+			font-size: 5em;
+		}
+
+		.start-btn {
+			margin-top: 40px;
+		}
+		.btn {
+			font: 1em;
+			font-weight: bold;
+			padding: 6px 12px;
+			border-radius: 10px;
+			height: 50px;
+			background: none;
+		}
+	}
+</style>
