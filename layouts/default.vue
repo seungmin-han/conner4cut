bdc4a974a415f4177591d6def08ee02eb25046d7
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-05-17 16:15:56 +0900

    Feat: 기본 레이아웃 구성(푸터 임시 제외)

---
+++ layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+	<div id="wrapper">
+		<div id="content">
+			<slot />
+		</div>
+		<!-- <Footer></Footer> -->
+	</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss">
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		list-style: none;
+		text-decoration: none;
+	}
+
+	body {
+		background-image: url('~/assets/images/paper3.jpg');
+		background-size: contain;
+		// font-family: Papyrus;
+		font-family: Viner Hand ITC;
+	}
+</style>
+<style lang="scss" scoped>
+	#wrapper {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		height: 100vh;
+
+		#content {
+			flex: 1;
+		}
+	}
+</style>
