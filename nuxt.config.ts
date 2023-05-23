8a2e47d9c43bec8cdbe2e8a55ed860aabd715da2
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-05-23 17:35:41 +0900

    Update: head 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,18 @@ export default defineNuxtConfig({
         '@pinia/nuxt',
         '@vueuse/nuxt',
     ],
+    app: {
+        head: {
+            script: [
+                {
+                    src: "https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js",
+			        integrity:"sha384-dpu02ieKC6NUeKFoGMOKz6102CLEWi9+5RQjWSV0ikYSFFd8M3Wp2reIcquJOemx",
+                    crossorigin: "anonymous"
+                }
+		
+            ]
+        }
+    },
 
     runtimeConfig: {
         public: {
