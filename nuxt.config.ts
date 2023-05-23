e50177a83e470a1f04d0132c8c45543b3757d40b
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-05-23 16:08:21 +0900

    Update: firebase test를 위한 변수 선언

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,21 @@ export default defineNuxtConfig({
         '@pinia/nuxt',
         '@vueuse/nuxt',
     ],
+
+    runtimeConfig: {
+        public: {
+            firebaseConfig: {
+                apiKey: "AIzaSyACCN2_1ZMB1GliF9x4sRHxbqj7G2IFbXQ",
+                authDomain: "conner4cuts.firebaseapp.com",
+                projectId: "conner4cuts",
+                storageBucket: "conner4cuts.appspot.com",
+                messagingSenderId: "223296145979",
+                appId: "1:223296145979:web:2fca5e746a27c3a1ed2a1e",
+                measurementId: "G-42808G7FQ5",
+                databaseURL: "https://conner4cuts-default-rtdb.firebaseio.com/",
+              }
+        }
+        
+    },
     ssr: false,
 })
