0d3d84b5e3e5c14078e58c63d2e1f1cd1b74857f
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-05-24 11:42:21 +0900

    Feat: 로그인 성공 후 토큰 발급 페이지

---
+++ pages/success.vue
@@ -0,0 +1,33 @@
+<template>
+	<div></div>
+</template>
+
+<script setup>
+	import axios from 'axios';
+	const route = useRoute();
+	const code = route.query.code;
+	console.log(code);
+
+	axios
+		.post(
+			'https://kauth.kakao.com/oauth/token',
+			{
+				client_id: useRuntimeConfig().public.kakaoConfig.restApiKey,
+				redirect_uri: 'http://localhost:3000/success',
+				grant_type: 'authorization_code',
+				code: code,
+			},
+			{
+				headers: {
+					'Content-Type': `application/x-www-form-urlencoded`,
+				},
+			}
+		)
+		.then(res => {
+			console.log(res);
+			opener.sessionStorage.setItem('token', res.data.access_token);
+			close();
+		});
+</script>
+
+<style lang="scss" scoped></style>
