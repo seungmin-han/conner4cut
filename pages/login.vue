07e43b95b03da91860577a3f1c963c0c9c944eb7
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-05-24 13:01:25 +0900

    Fix: vercel환경에서 돌아가도록 수정

--- pages/login.vue
+++ pages/login.vue
@@ -4,7 +4,7 @@
 
 <script setup>
 	Kakao.init(useRuntimeConfig().public.kakaoConfig.jsKey);
-	Kakao.Auth.authorize({ redirectUri: 'http://localhost:3000/success' });
+	Kakao.Auth.authorize({ redirectUri: `${window.location.origin}/success` });
 	// Kakao.API.request({ url: '/v2/user/me' });
 </script>
 
