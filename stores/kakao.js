6db909ce82036adb825ebb11cd3250d56e2089dd
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-05-24 11:43:46 +0900

    Feat: 카카오 고유 식별 번호를 저장하는 상태관리

---
+++ stores/kakao.js
@@ -0,0 +1,14 @@
+import { defineStore } from "pinia";
+
+
+export const useKakaoStore = defineStore('kakaoStore', {
+    state: () => ({
+        id: '',
+    }),
+    getters: {
+        
+    },
+    actions: {
+        
+    }
+})
