3151fb7e9ceeb0a849446eab75190b3ecab18eef
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-05-24 11:42:06 +0900

    Feat: 로그인 팝업을 위한 페이지

---
+++ pages/login.vue
@@ -0,0 +1,11 @@
+<template>
+	<div></div>
+</template>
+
+<script setup>
+	Kakao.init(useRuntimeConfig().public.kakaoConfig.jsKey);
+	Kakao.Auth.authorize({ redirectUri: 'http://localhost:3000/success' });
+	// Kakao.API.request({ url: '/v2/user/me' });
+</script>
+
+<style lang="scss" scoped></style>
