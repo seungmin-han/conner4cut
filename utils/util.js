77c9312aa49c41fe8c2ec0d580aca21015444599
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-05-24 17:03:05 +0900

    Feat: 카카오 로그인, 프로필 정보 조회

--- utils/util.js
+++ utils/util.js
@@ -1,4 +1,5 @@
 import { gsap } from "gsap"
+import { useKakaoStore } from "~/stores/kakao";
 
 const fadeIn = async (target = '.main' ,callback = ()=>{}) => {
     await gsap.from(target, {
@@ -16,4 +17,44 @@ const fadeOut = async (target = '.main', callback = ()=>{}) => {
     }).then(callback);
 }
 
-export { fadeIn, fadeOut }
+const kakaoLogin = () => {
+    if (!Kakao.isInitialized()) {
+        Kakao.init(useRuntimeConfig().public.kakaoConfig.jsKey);
+    }
+    const win = window.open('/login', 'login', '_top', 'width=300,height=500');
+    const timer = setInterval(() => {
+        if (win.closed) {
+            clearInterval(timer);
+            try {
+                Kakao.Auth.setAccessToken(sessionStorage.getItem('token'));
+                getKakaoProfile().then(res => {
+                    const kakaoStore = useKakaoStore();
+                    kakaoStore.id = res.id;
+                    kakaoStore.account = res.kakao_account;
+
+                    console.log(kakaoStore.id, kakaoStore.account);
+                })
+            } catch (e) {
+                console.log(e);
+                alert('로그인에 실패했습니다.',e);
+            }
+            
+        }
+    }, 1000);
+}
+
+const getKakaoProfile = () => {
+    return new Promise(
+        resolve => Kakao.API.request(
+            {
+                url: '/v2/user/me',
+                data: { property_keys: ['kakao_account.profile'] },
+            })
+            .then(res => {
+                resolve(res);
+
+            })
+    );
+}
+
+export { fadeIn, fadeOut, kakaoLogin }
