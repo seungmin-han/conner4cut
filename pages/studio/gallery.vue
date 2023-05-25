74d576df0eeb30cef8f2b2e1b70e96cd5d64ff40
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-05-25 15:17:02 +0900

    Feat: 갤러리 페이지 구현

--- pages/studio/gallery.vue
+++ pages/studio/gallery.vue
@@ -1,14 +1,20 @@
 <template>
 	<div class="main">
 		<div class="container">
-			<div v-for="user in list">
-				<div v-for="photo in user">
-					<img
-						:src="photo.href"
-						alt=""
-					/>
+			<template v-for="user in list">
+				<div
+					v-for="photo in user"
+					class="photo_wrap"
+				>
+					<div class="image">
+						<img
+							:src="photo.href"
+							alt=""
+						/>
+					</div>
+					<div class="shadow"></div>
 				</div>
-			</div>
+			</template>
 		</div>
 	</div>
 </template>
@@ -17,30 +23,55 @@
 	import { getDatabase, ref as fref, child as fchild, get as fget } from 'firebase/database';
 	import { initializeApp } from 'firebase/app';
 	import { useKakaoStore } from '~/stores/kakao';
+
 	const config = useRuntimeConfig();
 	const app = initializeApp(config.public.firebaseConfig);
+	const router = useRouter();
 	const db = getDatabase(app);
 	let list = ref({});
-
 	const store = useKakaoStore();
-	onMounted(() => {
-		if (!store?.id) {
-		}
+	const route = useRoute();
 
-		fget(fchild(fref(db), `photo/${store.id}`))
+	onMounted(() => {
+		if (route.query?.isMain) {
+			fadeIn('.main, .profile');
+		} else {
+			fadeIn();
+		}
+		fget(fchild(fref(db), `photo`))
 			.then(res => {
 				list.value = res.val();
 				console.log(list.value);
 			})
 			.catch(() => {
 				alert('잘못된 접근입니다.');
-				useRouter().replace('/');
+				router.replace('/');
 			});
 	});
 </script>
 
 <style lang="scss" scoped>
 	.main {
-		font-family: sans-serif;
+		.container {
+			display: flex;
+			.photo_wrap {
+				position: relative;
+				margin: 10px;
+				.image {
+					img {
+						display: block;
+					}
+				}
+				.shadow {
+					z-index: -1;
+					position: absolute;
+					top: 0;
+					left: 0;
+					width: 100%;
+					height: 100%;
+					box-shadow: 0px 0px 10px;
+				}
+			}
+		}
 	}
 </style>
